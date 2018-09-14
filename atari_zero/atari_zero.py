65126948b91db49ebc7070571ba5413ddd086689
Thibault Allançon <tallancon@gmail.com>
2018-09-14 10:04:57 +0200

    add frame preprocessing (grayscale, downsample)

--- atari_zero/atari_zero.py
+++ atari_zero/atari_zero.py
@@ -1,4 +1,15 @@
 import gym
+import numpy as np
+import skimage.color, skimage.transform
+
+
+def preprocess_frame(frame):
+    gray_frame = skimage.color.rgb2gray(frame)
+    resized_frame = skimage.transform.resize(gray_frame, [84, 84])
+    # Store on smallest type possible (uint8) because replay experience will
+    # take huge amount of memory, hence using float is very costly
+    compact_frame = np.uint8(resized_frame * 255)
+    return compact_frame
 
 
 env = gym.make('BreakoutDeterministic-v4')
