31a642e45769f438fbd9ccd35b4d971ac23ee33d
Thibault Allançon <tallancon@gmail.com>
2018-09-21 17:41:56 +0200

    add DQNAgent class

--- atari_zero/atari_zero.py
+++ atari_zero/atari_zero.py
@@ -5,6 +5,61 @@ import skimage.transform
 import keras
 
 
+class DQNAgent:
+    def __init__(self):
+        self.state_size = (84, 84, 4)
+        self.action_size = 3
+
+        self.build_atari_model()
+
+    def build_atari_model(self):
+        frames_input = keras.layers.Input(self.state_size, name='frames')
+        actions_input = keras.layers.Input((self.action_size, ), name='mask')
+
+        normalized = keras.layers.Lambda(lambda x: x / 255.)(frames_input)
+
+        layer1 = keras.layers.convolutional.Conv2D(
+            16, (8, 8), strides=(4, 4), activation='relu'
+        )(normalized)
+        layer2 = keras.layers.convolutional.Conv2D(
+            32, (4, 4), strides=(2, 2), activation='relu'
+        )(layer1)
+        flat_layer2 = keras.layers.core.Flatten()(layer2)
+        layer3 = keras.layers.Dense(256, activation='relu')(flat_layer2)
+
+        output = keras.layers.Dense(self.action_size)(layer3)
+        filtered_output = keras.layers.Multiply(name="Q_values")(
+            [output, actions_input]
+        )
+
+        self.model = keras.models.Model(
+            inputs=[frames_input, actions_input], outputs=filtered_output
+        )
+        self.optimizer = keras.optimizers.RMSprop(
+            lr=0.00025, rho=0.95, epsilon=0.01
+        )
+        self.model.compile(self.optimizer, loss='mse')
+
+    def fit_single_batch(self, gamma, actions, rewards,
+                         start_states, next_states, is_terminal):
+        # Predict Q values of the next states
+        next_Q_values = self.model.predict(
+            [next_states, np.ones(actions.shape)]
+        )
+        next_Q_values[is_terminal] = 0
+
+        # Calculate Q values of start states
+        Q_values = rewards + gamma * np.max(next_Q_values, axis=1)
+
+        # Fit Keras model
+        training_data = [start_states, actions]
+        target_data = actions * Q_values[:, None]
+        self.model.fit(
+            training_data, target_data,
+            nb_epoch=1, batch_size=len(start_states), verbose=0
+        )
+
+
 def preprocess_frame(frame):
     gray_frame = skimage.color.rgb2gray(frame)
     resized_frame = skimage.transform.resize(gray_frame, [84, 84])
@@ -14,56 +69,9 @@ def preprocess_frame(frame):
     return compact_frame
 
 
-def fit_single_batch(model, gamma,
-                     start_states, actions, rewards, next_states, is_terminal):
-    # Predict Q values of the next states
-    next_Q_values = model.predict([next_states, np.ones(actions.shape)])
-    next_Q_values[is_terminal] = 0
-
-    # Calculate Q values of start states
-    Q_values = rewards + gamma * np.max(next_Q_values, axis=1)
-
-    # Fit Keras model
-    training_data = [start_states, actions]
-    target_data = actions * Q_values[:, None]
-    model.fit(
-        training_data, target_data,
-        nb_epoch=1, batch_size=len(start_states), verbose=0
-    )
-
-
-def atari_model():
-    ATARI_SHAPE = (84, 84, 4)
-    NB_ACTIONS = 3
-
-    frames_input = keras.layers.Input(ATARI_SHAPE, name='frames')
-    actions_input = keras.layers.Input((NB_ACTIONS, ), name='mask')
-
-    normalized = keras.layers.Lambda(lambda x: x / 255.)(frames_input)
-
-    layer1 = keras.layers.convolutional.Conv2D(
-        16, (8, 8), strides=(4, 4), activation='relu'
-    )(normalized)
-    layer2 = keras.layers.convolutional.Conv2D(
-        32, (4, 4), strides=(2, 2), activation='relu'
-    )(layer1)
-    flat_layer2 = keras.layers.core.Flatten()(layer2)
-    layer3 = keras.layers.Dense(256, activation='relu')(flat_layer2)
-
-    output = keras.layers.Dense(NB_ACTIONS)(layer3)
-    filtered_output = keras.layers.Multiply(name="Q_values")(
-        [output, actions_input]
-    )
-
-    model = keras.models.Model(
-        inputs=[frames_input, actions_input], outputs=filtered_output
-    )
-    optimizer = keras.optimizers.RMSprop(lr=0.00025, rho=0.95, epsilon=0.01)
-    model.compile(optimizer, loss='mse')
-    return model
-
-
 env = gym.make('BreakoutDeterministic-v4')
+agent = DQNAgent()
+
 env.reset()
 env.render()
 
@@ -71,5 +79,5 @@ is_done = False
 while not is_done:
     frame, reward, is_done, _ = env.step(env.action_space.sample())
     preprocess_frame(frame)
-    atari_model()
+    agent.build_atari_model()
     env.render()
